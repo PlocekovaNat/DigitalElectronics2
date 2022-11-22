0cc4b3dd08cfc8477425fdf04bd4fd0f02641e6a
PlocekovaNat <xploce02@vutbr.cz>
2022-11-22 14:49:32 +0100

    projetk

---
+++ projekt/Projekt1/src/main.c
@@ -0,0 +1,167 @@
+/***********************************************************************
+ * 
+ * Stopwatch by Timer/Counter2 on the Liquid Crystal Display (LCD)
+ *
+ * ATmega328P (Arduino Uno), 16 MHz, PlatformIO
+ *
+ * Copyright (c) 2017 Tomas Fryza
+ * Dept. of Radio Electronics, Brno University of Technology, Czechia
+ * This work is licensed under the terms of the MIT license.
+ * 
+ * Components:
+ *   16x2 character LCD with parallel interface
+ *     VSS  - GND (Power supply ground)
+ *     VDD  - +5V (Positive power supply)
+ *     Vo   - (Contrast)
+ *     RS   - PB0 (Register Select: High for Data transfer, Low for Instruction transfer)
+ *     RW   - GND (Read/Write signal: High for Read mode, Low for Write mode)
+ *     E    - PB1 (Read/Write Enable: High for Read, falling edge writes data to LCD)
+ *     D3:0 - NC (Data bits 3..0, Not Connected)
+ *     D4   - PD4 (Data bit 4)
+ *     D5   - PD5 (Data bit 5)
+ *     D6   - PD6 (Data bit 6)
+ *     D7   - PD7 (Data bit 7)
+ *     A+K  - Back-light enabled/disabled by PB2
+ * 
+ **********************************************************************/
+
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>         // AVR device-specific IO definitions
+#include <avr/interrupt.h>  // Interrupts standard C library for AVR-GCC
+#include <gpio.h>           // GPIO library for AVR-GCC
+#include "timer.h"          // Timer library for AVR-GCC
+#include <lcd.h>            // Peter Fleury's LCD library
+#include <stdlib.h>         // C library. Needed for number conversions
+
+
+/* Function definitions ----------------------------------------------*/
+/**********************************************************************
+ * Function: Main function where the program execution begins
+ * Purpose:  Update stopwatch value on LCD screen when 8-bit 
+ *           Timer/Counter2 overflows.
+ * Returns:  none
+ **********************************************************************/
+
+// Rotary Encoder Inputs
+#define CLK 2
+#define DT 3
+#define SW 4
+
+int counter = 0;
+int currentStateCLK;
+int lastStateCLK;
+String currentDir ="";
+unsigned long lastButtonPress = 0;
+
+
+int main(void)
+{
+    uint8_t customChar[16] = {
+        0b00111,
+        0b01110,
+        0b11100,
+        0b11000,
+        0b11100,
+        0b01110,
+        0b00111,
+        0b00011,
+        //Second charakter
+        0b00111,
+        0b01110,
+        0b11100,
+        0b11000,
+        0b11100,
+        0b01110,
+        0b00111,
+        0b00011
+    };
+
+    // Initialize display
+    lcd_init(LCD_DISP_ON_CURSOR);
+
+    lcd_command(1<<LCD_CGRAM);       // Set addressing to CGRAM (Character Generator RAM)
+                                     // ie to individual lines of character patterns
+    for (uint8_t i = 0; i < 16; i++)  // Copy new character patterns line by line to CGRAM
+        lcd_data(customChar[i]);
+    lcd_command(1<<LCD_DDRAM);       // Set addressing back to DDRAM (Display Data RAM)
+                                     // ie to character codes
+    lcd_gotoxy(13,1);
+    // Display symbol with Character code 0
+    //lcd_putc(0x00);
+    //lcd_putc(0x01);
+
+
+    // Put string(s) on LCD screen
+    lcd_gotoxy(5, 0);
+    lcd_putc('a');
+
+
+
+    // Configuration of 8-bit Timer/Counter2 for Stopwatch update
+    // Set the overflow prescaler to 16 ms and enable interrupt
+
+    // Infinite loop
+    while (1)
+    {
+        /* Empty loop. All subsequent operations are performed exclusively 
+         * inside interrupt service routines, ISRs */
+    }
+
+    // Will never reach this
+    return 0;
+
+    // Set encoder pins as inputs
+	pinMode(CLK,PB4);
+	pinMode(DT,PB3);
+	pinMode(SW, PB2);
+
+
+	// Read the initial state of CLK
+	lastStateCLK = digitalRead(CLK);
+    // Read the current state of CLK
+	currentStateCLK = digitalRead(CLK);
+
+	// If last and current state of CLK are different, then pulse occurred
+	// React to only 1 state change to avoid double count
+	if (currentStateCLK != lastStateCLK  && currentStateCLK == 1){
+
+		// If the DT state is different than the CLK state then
+		// the encoder is rotating CCW so decrement
+		if (digitalRead(DT) != currentStateCLK) {
+			counter --;
+			currentDir ="CCW";
+		} else {
+			// Encoder is rotating CW so increment
+			counter ++;
+			currentDir ="CW";
+		}
+
+    lcd_puts("Direction: ");
+    lcd_puts(currentDir);
+    lcd_puts(" | Counter: ");
+    lcd_puts(currentDir);
+
+
+    // Remember last CLK state
+	lastStateCLK = currentStateCLK;
+
+	// Read the button state
+	int btnState = digitalRead(SW);
+
+	//If we detect LOW signal, button is pressed
+	if (btnState == LOW) {
+		//if 50ms have passed since last LOW pulse, it means that the
+		//button has been pressed, released and pressed again
+		if (millis() - lastButtonPress > 50) {
+			lcd_puts("Button pressed! ");
+		}
+
+		// Remember last button press event
+		lastButtonPress = millis();
+	}
+
+	// Put in a slight delay to help debounce the reading
+	delay(1);
+	}
+
+}
